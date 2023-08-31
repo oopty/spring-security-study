e4f9afd5f4c40b58e80b0a745f567cd6662090a9
oopty <tiramisu13795@gmail.com>
2023-08-31 23:59:48 +0900

    [feat]: Add RemeberMe feature

--- src/main/java/me/oopty/springsecuritystudy/SpringSecurityStudyApplication.java
+++ src/main/java/me/oopty/springsecuritystudy/SpringSecurityStudyApplication.java
@@ -3,13 +3,15 @@ package me.oopty.springsecuritystudy;
 import me.oopty.springsecuritystudy.config.formLogin.FormLoginSecurityConfig;
 import me.oopty.springsecuritystudy.config.httpBasic.HttpBasicSecurityConfig;
 import me.oopty.springsecuritystudy.config.logout.LogoutSecurityConfig;
+import me.oopty.springsecuritystudy.config.rememberMe.RememberMeSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(HttpBasicSecurityConfig.class)
 //@Import(FormLoginSecurityConfig.class)
-@Import(LogoutSecurityConfig.class)
+//@Import(LogoutSecurityConfig.class)
+@Import(RememberMeSecurityConfig.class)
 @SpringBootApplication(scanBasePackages = "me.oopty.springsecuritystudy.mvc")
 public class SpringSecurityStudyApplication {
 
---
+++ src/main/java/me/oopty/springsecuritystudy/config/rememberMe/RememberMeSecurityConfig.java
@@ -0,0 +1,25 @@
+package me.oopty.springsecuritystudy.config.rememberMe;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class RememberMeSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+
+        http
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .alwaysRemember(true);
+    }
+}
